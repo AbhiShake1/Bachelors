61d543d6897394ff843a2f5b8038caddf8eb91b4
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2021-07-07 13:19:18 +0545

    fix logical error

--- 2nd-sem/ComputingFundamentals/Week3Lab/AverageMarks.py
+++ 2nd-sem/ComputingFundamentals/Week3Lab/AverageMarks.py
@@ -12,22 +12,22 @@ print(f"Name of student: {name}")
 print(f"Marks: {m1}, {m2}, {m3}, {m4}, {m5}")
 print(f"Average marks: {average}")
 
-if average <= 100 and average > 70:
+if average <= 100 and average >= 70:
     print("Grade A")
 
-elif average <= 69 and average > 60:
+elif average <= 69 and average >= 60:
     print("Grade B")
 
-elif average <= 59 and average > 50:
+elif average <= 59 and average >= 50:
     print("Grade C")
 
-elif average <= 49 and average > 43:
+elif average <= 49 and average >= 43:
     print("Grade D")
 
-elif average <= 42 and average > 40:
+elif average <= 42 and average >= 40:
     print("Grade E")
 
-elif average <= 39 and average > 0:
+elif average <= 39 and average >= 0:
     print("Grade F")
 
 else:
