407965d927ae4ba6cda8643d0ac726c98f80a57c
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2021-07-07 19:42:36 +0545

    add components inside panel, leave comments to explain what was done

--- 2nd-sem/Programming/Week18/Workshop/Form.java
+++ 2nd-sem/Programming/Week18/Workshop/Form.java
@@ -1,19 +1,38 @@
-import javax.swing.*;
+import javax.swing.*; //import all classes from swing package
+//class that sets font family, style, size and other property
+//as per our needs
 import java.awt.Font;
+//class that defines width and height. setPreferredSize function
+//takes this class as an argument
+import java.awt.Dimension;
 
-public class Form{
-    private static JFrame frame;
+/*
+ * package private(default access modifier) class
+ * sets up UI for form
+ */
+class Form{
+    //stores panel object
+    //global variable so that it can be accessed from all methods
+    //in this class
+    private static JPanel panel;
+
     public static void main(String[] args){
-        frame = new JFrame();
-        //labels
+        //create main/root frame
+        JFrame frame = new JFrame();
+        //create a panel to store components
+        panel = new JPanel();
+        //null layout manager to make position absolute 
+        //instead of relative
+        panel.setLayout(null);
+        //add labels. see method description for more details
         setLabel("Register User",20,220,0,500,100);
         setLabel("First name",15,10,50,100,100);
         setLabel("Last name",15,10,120,100,100);
-        setLabel("Last name",15,10,190,100,100);
+        setLabel("Email",15,10,190,100,100);
         setLabel("Username",15,300,50,100,100);
         setLabel("Password",15,300,120,100,100);
         setLabel("Mobile number",15,300,190,200,100);
-        //text fields        
+        //add text fields. see method description for more details
         setTextField(100,90,130,20);
         setTextField(100,160,130,20);
         setTextField(100,230,130,20);
@@ -22,27 +41,54 @@ public class Form{
         setTextField(420,230,130,20);
         //buttons
         JButton registerBtn = new JButton("Register");
+        //x, y coordinates, width, height
         registerBtn.setBounds(230,280,120,40);
-        frame.add(registerBtn);
-        
-        //600 width 400 height
-        frame.setSize(600,400);
-        //to set absoulte position for components
-        frame.getContentPane().setLayout(null); 
+        //add button with its set properties
+        panel.add(registerBtn);
+        //add panel with all its properties and components added 
+        //inside it to the main frame
+        frame.add(panel);
+        //600 width 350 height
+        //setPreferredSize was used instead of setLayoutSize
+        //because parent layout manager(of frame) exists and
+        //setSize would not allow to use frame.pack() normally
+        panel.setPreferredSize(new Dimension(600,350));
+        //set size of frame as per preferred size of components inside
+        frame.pack();
+        //terminate the program when app is closed
+        //only hides the app by default
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //show frame. hidden by default
         frame.setVisible(true);
     }
-    
+
+    /*
+     * @params: name of label, font size, x coordinate, y coordinate
+     * width, height
+     * adds a label as per arguments
+     * @returns void
+     */
     private static void setLabel(String name, int fontSize, int...bounds){
-        JLabel label = new JLabel(name);
+        JLabel label = new JLabel(name);//create a label
+        //set coordinates and sizes as per first 4 arguments to bounds
         label.setBounds(bounds[0],bounds[1],bounds[2],bounds[3]);
-        label.setFont(new Font("Sans", Font.PLAIN, fontSize));
-        frame.add(label);
+        //default font faimly and style, font size as per argument
+        label.setFont(new Font(null, 0, fontSize));
+        //add the created label to global panel component
+        panel.add(label);
     }
-    
+
+    /*
+     * @params: x coordinate, y coordinate, width, height
+     * adds a text field as per arguments
+     * @returns void
+     */
     private static void setTextField(int...bounds){
+        //create text field where user can write/fill data inside
         JTextField textField = new JTextField();
+        //set coordinates and sizes as per first 4 arguments
         textField.setBounds(bounds[0],bounds[1],bounds[2],bounds[3]);
-        frame.add(textField);
+        //add the created text field component to global panel component
+        panel.add(textField);
     }
 }
