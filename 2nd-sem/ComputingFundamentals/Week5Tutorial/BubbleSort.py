9e2f3ceabb859e32cbf7efa51b65a64547900e0f
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2021-07-19 13:52:11 +0545

    .

---
+++ 2nd-sem/ComputingFundamentals/Week5Tutorial/BubbleSort.py
@@ -0,0 +1,21 @@
+unsortedArray = []
+
+length = int(input("Enter length of array: "))
+for _ in range(length):
+    i = int(input("Enter the element: "))
+    unsortedArray.append(i)
+
+unsorted = True
+
+while unsorted:
+    nSwap = 0
+    for i in range(1, len(unsortedArray)):
+        if unsortedArray[i-1] > unsortedArray[i]:
+            unsortedArray[i-1], unsortedArray[i] = unsortedArray[i], unsortedArray[i-1] #swap
+            nSwap += 1
+        if nSwap == 0:
+            unsorted = False #break loop
+
+#output
+[print(i) for i in unsortedArray] #sorted array
+        
